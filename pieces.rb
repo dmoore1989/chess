afb7e07780d7860bfb9e00deaead1942e9608bce
Douglas Moore <douglas.moore89@gmail.com>
2015-10-05 14:57:51 -0400

    Begin building individual chess pieces

--- pieces.rb
+++ pieces.rb
@@ -8,4 +8,30 @@ class Piece
     @value.to_s
   end
 
+  def moves
+
+  end
+
+  def position
+  end
+
+
+end
+
+class SlidingPiece < Piece
+
+end
+
+class SteppingPiece < Piece
+
+end
+
+class Knight < SteppingPiece
+  COORDINATES = [
+    [2,1], [1,2], [-1, -2], [-2, -1], [-1, 2], [-2, 1], [1, -2], [2, -1]
+  ]
+  def movedirs
+    COORDINATES.each{ |c| }
+  end
+
 end
