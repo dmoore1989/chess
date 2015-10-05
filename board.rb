d14dadc435967c98d75983901fc30b7c9e110d49
Douglas Moore <douglas.moore89@gmail.com>
2015-10-05 12:30:42 -0400

    Started error exception testing

--- board.rb
+++ board.rb
@@ -5,7 +5,27 @@ class Board
   end
 
   def place_pieces
+    @grid.each_with_index |row, i|
+      if i < 2 || i > 5
+        row.each{ |el| @grid << Piece.new }
+      end
+    end
+  end
+
+  class ChessError << StandardError
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y] 
+  end
+
+  def start_test(start)
+    raise ChessError.new "No piece at start point" if
+  end
+
+  def move(start, end)
 
   end
-  
+
 end
